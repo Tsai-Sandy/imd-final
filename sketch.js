9f10d38cfaefab2bdd59ddcd74301ce5ff92ef36
Tsai-Sandy <sandy51529@gmail.com>
2022-12-30 11:32:16 +0800

    feat: put book on shelf

--- sketch.js
+++ sketch.js
@@ -1,8 +1,5 @@
-let bookshelf;
-let add_button;
+let bookshelf, add_button;
 let size = getSize();
-let input_title;
-let button_title;
 let books = [];
 let books_back = [];
 let books_x = [];
@@ -11,6 +8,8 @@ let books_a = [];
 let gravity = 0.5;
 let isDrag = false;
 let nowDrag = null;
+let inShelf = [];
+let putBook = [];
 
 function preload() {
   bookshelf = loadImage("images/book_shelf.png");
@@ -31,13 +30,17 @@ function draw() {
   background(254, 223, 225);
   image(bookshelf, size[0] / 2, size[1] * 3 / 4, size[1] / 2, size[1] / 2);
   for(let i = 0; i < books.length; i++) {
-    if (books_y[i] <= size[1] - 100){
-      if (!isDrag) {
+    if (books_y[i] <= size[1] - size[1] / 8){
+      if (!isDrag && !putBook[i]) {
         books_a[i] += gravity;
         books_y[i] += books_a[i];
       }
     }
-    image(books[i], books_x[i], books_y[i], size[0]/10, size[0]/10); // 需再調整大小，等書籍的圖片確定後再說
+    if (!inShelf[i]){
+      image(books[i], books_x[i], books_y[i], size[0] / 11, size[0] / 11 * 1.05); // 需再調整大小，須確定圖片
+    } else {
+      image(books_back[i], books_x[i], books_y[i], size[1] / 8 * 0.1, size[1] / 8);
+    } 
   }
 }
 
@@ -45,8 +48,8 @@ function hideAddButton() {
   add_button.position(-100, -100);
   var isbn = prompt("ISBN????");
   if (isbn !== null) {
-    hideInput(isbn)
-  }else {
+    hideInput(isbn);
+  } else {
     add_button.position(size[0] / 2 - size[1] / 16, size[1] / 3 + size[1] / 16);
   }
 }
@@ -60,33 +63,44 @@ function hideInput(isbn) {
     alert("查無書籍");
   } else {
     drawBook(isbn);
-    getBookData(isbn)
+    getBookData(isbn);
   }
 }
 
 function drawBook(isbn) {
-  console.log(`${isbn}.jpg`);
+  x = random(1, size[0]);
+  y = -10;
+
   books.push(loadImage(`./images/books/${isbn}.png`));
   books_back.push(loadImage(`./images/books/${isbn}_back.png`))
-  let x = random(0, size[0])
   books_x.push(x);
-  books_y.push(-10);
+  books_y.push(y);
   books_a.push(0);
+  inShelf.push(false);
+  putBook.push(false);
 }
 
 function getSize() {
   var body = document.body, html = document.documentElement;
+  var screen_width = Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth);
+  var screen_height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
 
-  var width = Math.max(body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth);
-  var height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-
-  return [width, height]
+  return [screen_width, screen_height]
 }
 
 function mouseDragged() {
   for (let i = 0; i < books.length; i++) {
-    if ((mouseX > books_x[i] - 50) && (mouseX < books_x[i] + 50)) {
-      if ((mouseY > books_y[i] - 50) && (mouseY < books_y[i] + 50)) {
+    if ((mouseX > books_x[i] - size[0] / 22) && (mouseX < books_x[i] + size[0] / 22)) {
+      if ((mouseY > books_y[i] - size[0] / 22) && (mouseY < books_y[i] + size[0] / 22)) {
+        if ((mouseX > size[0] / 2  - size[1] / 4) && (mouseX < size[0] / 2  + size[1] / 4)){
+          if ((mouseY > size[1] * 3 / 4  - size[1] / 4) && (mouseY < size[1] * 3 / 4  + size[1] / 4)) {
+            inShelf[i] = true;
+          } else {
+            inShelf[i] = false;
+          }
+        } else {
+          inShelf[i] = false;
+        }
         isDrag = true;
         nowDrag = i;
         books_x[i] = mouseX;
@@ -97,10 +111,20 @@ function mouseDragged() {
 }
 
 function mouseReleased() {
-  isDrag = false;
   if (nowDrag != null) {
     books_a[nowDrag] = 0;
+  } 
+  if ((mouseX > size[0] / 2  - size[1] / 4) && (mouseX < size[0] / 2  + size[1] / 4)) {
+    if ((mouseY > size[1] * 3 / 4  - size[1] / 4) && (mouseY < size[1] * 3 / 4  + size[1] / 4)) {
+      putBook[nowDrag] = true;
+    } else {
+      putBook[nowDrag] = false;
+    }
+  } else {
+    putBook[nowDrag] = false;
   }
+  
+  isDrag = false;
   nowDrag = null;
 }
 
@@ -114,7 +138,6 @@ function checkData(isbn) {
   req = new XMLHttpRequest();
   req.open('GET', `images/books/${isbn}.png`, false);
   req.send();
-  console.log(req.status);
   if (req.status === 200)
     return true;
   else
