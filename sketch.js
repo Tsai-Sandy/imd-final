5db7bc4a60f193385702427db536dda80938b888
Lamuyang <lamuyangyang@gmail.com>
2022-12-30 13:21:21 +0800

    update

--- sketch.js
+++ sketch.js
@@ -8,9 +8,13 @@ let books_a = [];
 let gravity = 0.5;
 let isDrag = false;
 let nowDrag = null;
+let isDrag_list = []
 let inShelf = [];
 let putBook = [];
-
+let isbn_list = [];
+let isDisplay = false;
+let data_list = [];
+let isClick_list = []
 function preload() {
   bookshelf = loadImage("images/book_shelf.png");
 }
@@ -31,16 +35,48 @@ function draw() {
   image(bookshelf, size[0] / 2, size[1] * 3 / 4, size[1] / 2, size[1] / 2);
   for(let i = 0; i < books.length; i++) {
     if (books_y[i] <= size[1] - size[1] / 8){
-      if (!isDrag && !putBook[i]) {
+      if (!isDrag_list[i] && !putBook[i]) {
         books_a[i] += gravity;
         books_y[i] += books_a[i];
+        isDisplay = false
       }
     }
     if (!inShelf[i]){
       image(books[i], books_x[i], books_y[i], size[0] / 11, size[0] / 11 * 1.05); // 需再調整大小，須確定圖片
     } else {
-      image(books_back[i], books_x[i], books_y[i], size[1] / 8 * 0.1, size[1] / 8);
-    } 
+      image(books_back[i], books_x[i], books_y[i], size[1] / 6 * 0.15, size[1] / 6);
+    }
+    if (isDisplay) {
+      display_data(isbn_list[i])
+    }
+  }
+}
+
+function display_data(isbn) {
+  for(let i = 0; i < data_list.length; i++) {
+    if (data_list[i].ISBN === isbn && isClick_list[i]) {
+      textSize(32);
+      text(data_list[i].Title, 10, 30);
+      fill(0, 102, 153);
+    }
+  }
+}
+
+function mousePressed() {
+  for (let i = 0; i < books.length; i++) {
+    if ((mouseX > books_x[i] - size[0] / 22) && (mouseX < books_x[i] + size[0] / 22)) {
+      if ((mouseY > books_y[i] - size[0] / 22) && (mouseY < books_y[i] + size[0] / 22)) {
+        if ((mouseX > size[0] / 2  - size[1] / 4) && (mouseX < size[0] / 2  + size[1] / 4)){
+          if ((mouseY > size[1] * 3 / 4  - size[1] / 4) && (mouseY < size[1] * 3 / 4  + size[1] / 4)) {
+            for(let z = 0; z < isClick_list.length; z++) {
+              isClick_list[z] = false
+            }
+          }
+          isClick_list[i] = true
+          isDisplay = true
+        }
+      }
+    }
   }
 }
 
@@ -64,6 +100,8 @@ function hideInput(isbn) {
   } else {
     drawBook(isbn);
     getBookData(isbn);
+    isDrag_list.push(false)
+    isClick_list.push(false)
   }
 }
 
@@ -71,6 +109,7 @@ function drawBook(isbn) {
   x = random(1, size[0]);
   y = -10;
 
+  isbn_list.push(isbn)
   books.push(loadImage(`./images/books/${isbn}.png`));
   books_back.push(loadImage(`./images/books/${isbn}_back.png`))
   books_x.push(x);
@@ -101,7 +140,10 @@ function mouseDragged() {
         } else {
           inShelf[i] = false;
         }
-        isDrag = true;
+        for(let z = 0; z < isDrag_list.length; z++) {
+          isDrag_list[z] = false
+        }
+        isDrag_list[i] = true;
         nowDrag = i;
         books_x[i] = mouseX;
         books_y[i] = mouseY;
@@ -124,7 +166,7 @@ function mouseReleased() {
     putBook[nowDrag] = false;
   }
   
-  isDrag = false;
+  isDrag_list[nowDrag] = false;
   nowDrag = null;
 }
 
@@ -132,6 +174,7 @@ async function getBookData(ISBN) {
   const response = await fetch(`http://127.0.0.1:8000/get_book/?ISBN=${ISBN}`)
   var data = await response.json()
   console.log(data)
+  data_list.push(data)
 }
 
 function checkData(isbn) {
